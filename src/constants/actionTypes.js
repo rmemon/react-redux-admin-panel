ba89338154a61133aa5a128bb2ca8c533126a9fd
shah-dhaval <dhaval2711@gmail.com>
2018-07-20 12:57:12 +0530

    progress event for post, put

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -2,3 +2,4 @@ export const ASYNC_START = 'ASYNC_START';
 export const ASYNC_END = 'ASYNC_END';
 export const APP_LOAD = 'APP_LOAD';
 export const REDIRECT = 'REDIRECT';
+export const PROGRESSEVENT = 'PROGRESSEVENT';
